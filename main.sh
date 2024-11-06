3a9f443f27b15282122c5f750d46c747a6fcf1a2
zladen <zladen1983@gmail.com>
2024-11-06 12:52:07 +0300

    updates ETH_INTERFACE

--- main.sh
+++ main.sh
@@ -8,12 +8,9 @@ clear
 
 echo "Устанавили шрифт setfont cyr-sun16"
 
-#ETH_INTERFACE=$('./ethernet_connection.sh')
-#echo SCRIPT_DIR="$(dirname "$0")"
-
-#ETH_INTERFACE=$(source "./ethernet_connection.sh")
 #Настрока соединения с сетью...
 
-echo ETH_INTERFACE="$(source "$(dirname "$0")/ethernet_connection.sh")"
+#echo "$(dirname "$0")/ethernet_connection.sh"
 
-export ETH_INTERFACE
+ETH_INTERFACE=$(bash "$(dirname "$0")/ethernet_connection.sh")
+echo "$ETH_INTERFACE"
